52e9e4ba396266e937b44282df22a0feef312935
ctcl-bregis <brayden.regis@gmail.com>
2025-01-02 19:23:24 -0500

    20250102-1

--- build.py
+++ build.py
@@ -1,8 +1,8 @@
-# StaticSite - CTCL 2024
+# StaticSite
 # File: build.py
 # Purpose: Read configuration file and build static site
 # Created: December 16, 2024
-# Modified: December 31, 2024
+# Modified: January 02, 2025
 
 import os
 import json
@@ -11,6 +11,7 @@ import markdown2
 import minify_html
 import pathlib
 import shutil
+import argparse
 
 from datetime import datetime
 from enum import Enum
@@ -79,23 +80,9 @@ class Theme(BaseModel):
     def __hash__(self):
         return hash((self.dispname, self.color, self.fgcolor, self.templates, self.enabled))
 
-class SiteConfigLogLevel(str, Enum):
-    info = "info"
-    debug = "debug"
-    warning = "warning"
-    error = "error"
-    critical = "critical"
-    fatal = "fatal"
-
 class SiteConfig(BaseModel):
     sitedomain: str
-    minifyhtml: bool
-    minifycss: bool
     dateformats: Dict[str, str]
-    loglevel: SiteConfigLogLevel
-    thumbnails: bool
-    thumbsize: int
-    skiperrors: bool
     # TODO: Value str should be an enum
     staticexts: Dict[str, str]
     themes: Dict[str, Theme]
@@ -126,8 +113,6 @@ def getdaterange(pageconfig: Page):
 
     if pageconfig.dateprecision == "day" and startdate.year == enddate.year and startdate.month == enddate.month and startdate.day == enddate.day:
         return str(startdate.strftime(siteconfig.dateformats["day"]))
-    
-
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.FATAL)
@@ -135,12 +120,69 @@ handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
 logger.addHandler(handler)
 
-try:
-    with open("cfgdir.txt", "r") as f:
-        cfgdir = f.read()
-except:
-    logger.fatal("Could not read cfgdir.txt")
+parser = argparse.ArgumentParser(description = "Static website generator", prog = "StaticSite")
+parser.add_argument("configdir", help = "Configuration (input) directory", type = str)
+parser.add_argument("outputdir", help = "Output directory", type = str)
+parser.add_argument("-l", "--loglevel", choices = ["info", "debug", "warning", "error", "fatal"], default = "info", help = "Verbosity level")
+parser.add_argument("-e", "--skiperrors", help = "Skip errors", type = bool, default = True)
+parser.add_argument("--minify-html", help = "Minify HTML", action = "store_true")
+parser.add_argument("--minify-css", help = "Minify CSS", action = "store_true")
+parser.add_argument("--thumb", help = "Generate thumbnails", action = "store_true")
+parser.add_argument("--thumb-size", help = "Max thumbnail height", type = int, default = 720)
+parser.add_argument("--thumb-algo", help = "Thumbnail resize algorithm", choices = ["nearest", "lanczos", "bilinear", "bicubic", "box", "hamming"], default = "box")
+
+args = parser.parse_args()
+cfgdir = args.configdir
+if not os.path.exists(cfgdir):
+    logger.fatal(f"Configuration directory {cfgdir} does not exist")
     exit(1)
+elif not os.path.isdir(cfgdir):
+    logger.fatal(f"{cfgdir} is not a directory")
+    exit(1)
+
+outdir = args.outputdir
+if not os.path.exists(outdir):
+    logger.fatal(f"Output directory {outdir} does not exist")
+    exit(1)
+elif not os.path.isdir(outdir):
+    logger.fatal(f"{outdir} is not a directory")
+    exit(1)
+
+loglevel = args.loglevel
+
+if loglevel == "debug":
+    logger.setLevel(logging.DEBUG)
+elif loglevel == "info":
+    logger.setLevel(logging.INFO)
+elif loglevel == "warning":
+    logger.setLevel(logging.WARNING)
+elif loglevel == "error":
+    logger.setLevel(logging.ERROR)
+elif loglevel == "critical":
+    logger.setLevel(logging.CRITICAL)
+elif loglevel == "fatal":
+    logger.setLevel(logging.FATAL)
+
+skiperrors = args.skiperrors
+minifyhtml = args.minify_html
+minifycss = args.minify_css
+thumbnails = args.thumb
+print(thumbnails)
+thumbsize = args.thumb_size
+
+thumbalgo = args.thumb_algo
+if thumbalgo == "nearest":
+    thumbalgo = Image.Resampling.NEAREST
+elif thumbalgo == "lanczos":
+    thumbalgo = Image.Resampling.LANCZOS
+elif thumbalgo == "bilinear":
+    thumbalgo = Image.Resampling.BILINEAR
+elif thumbalgo == "bicubic":
+    thumbalgo = Image.Resampling.BICUBIC
+elif thumbalgo == "box":
+    thumbalgo = Image.Resampling.BOX
+elif thumbalgo == "hamming":
+    thumbalgo = Image.Resampling.HAMMING
 
 configjsonpath = pathlib.Path(cfgdir) / "config.json"
 
@@ -153,30 +195,13 @@ except Exception as e:
     logger.fatal(f"Could not read config.json {e}")
     exit(1)
 
-if siteconfig.loglevel == "debug":
-    logger.setLevel(logging.DEBUG)
-elif siteconfig.loglevel == "info":
-    logger.setLevel(logging.INFO)
-elif siteconfig.loglevel == "warning":
-    logger.setLevel(logging.WARNING)
-elif siteconfig.loglevel == "error":
-    logger.setLevel(logging.ERROR)
-elif siteconfig.loglevel == "critical":
-    logger.setLevel(logging.CRITICAL)
-elif siteconfig.loglevel == "fatal":
-    logger.setLevel(logging.FATAL)
-
-if os.path.exists("./out"):
-    os.system("rm out")
-    os.system("rm -r /dev/shm/out/")
-    # This is here to prevent unnecessary writes to the disk
-    os.mkdir("/dev/shm/out/")
-    os.system(f"ln -s /dev/shm/out/ out")
-
 # Render pages
 templates = {}
 for templatedir in [x for x in os.listdir(os.path.join(cfgdir, "templates"))]:
-    templates[templatedir] = Environment(loader = FileSystemLoader(os.path.join(cfgdir, "templates", templatedir, "html")), autoescape = select_autoescape())
+    templates[templatedir] = Environment(
+        loader = FileSystemLoader(os.path.join(cfgdir, "templates", templatedir, "html")),
+        autoescape = select_autoescape()
+    )
 
 pages = [x[0] for x in os.walk(os.path.join(cfgdir, "pages"))]
 pageconfigs = {}
@@ -199,7 +224,7 @@ for page in pages:
             pageconfigjson = json.loads(pagejson)
         except Exception as e:
             logger.error(f"Could not parse {pagejsonpath} {e}")
-            if not siteconfig.skiperrors:
+            if not skiperrors:
                 exit(1)
             else:
                 continue
@@ -215,7 +240,7 @@ for page in pages:
         sectionmdpath = f"{page}/{pageconfig.content[content].content}"
         if not os.path.exists(sectionmdpath):
             logger.error(f"{sectionmdpath} does not exist")
-            if not siteconfig.skiperrors:
+            if not skiperrors:
                 exit(1)
             pageconfig.content[content].content = ""
         else:
@@ -229,34 +254,31 @@ for page in pages:
 logger.info(f"{len(pageconfigs)} pages found")
 
 for pagepath, pageconfig in pageconfigs.items():
-
     pagetemplate = templates[siteconfig.themes[pageconfig.theme].templates].get_template("main.lis")
     pagehtml = pagetemplate.render(page = pageconfig, pages = pageconfigs)
     if pagepath != "/":
-        os.makedirs("./out" + pagepath)
+        os.makedirs(os.path.join(outdir, pagepath.removeprefix("/")))
 
-    if siteconfig.minifyhtml:
+    if minifyhtml:
         pagehtml = minify_html.minify(pagehtml)
 
-    print(pagepath)
-    with open("./out" + pagepath + "index.html", "w") as f:
+    with open(outdir + pagepath + "index.html", "w") as f:
         f.write(pagehtml)
 
-
 # Copy static files
-shutil.copytree(os.path.join(cfgdir, "static"), "./out/static")
+shutil.copytree(os.path.join(cfgdir, "static"), os.path.join(outdir, "static"))
 
 pages = [x for x in os.walk(os.path.join(cfgdir, "pages"))]
 for page in pages:
     for f in page[2]:
         if pathlib.Path(f).suffix in siteconfig.staticexts:
-            pageurl = page[0].removeprefix(cfgdir + "/pages") + "/"
-            if not os.path.exists("./out/static/pages" + pageurl):
-                os.makedirs("./out/static/pages" + pageurl)
-            shutil.copy(os.path.join(page[0], f), "./out/static/pages" + pageurl + f)
+            pageurl = page[0].removeprefix(cfgdir + "/pages/") + "/"
+            if not os.path.exists(os.path.join(outdir, "static/pages", pageurl)):
+                os.makedirs(os.path.join(outdir, "static/pages", pageurl))
+            shutil.copy(os.path.join(page[0], f), os.path.join(outdir, "static/pages", pageurl, f))
 
-if siteconfig.thumbnails:
-    staticdirs = [x for x in os.walk("./out/static/pages/")]
+if thumbnails:
+    staticdirs = [x for x in os.walk(os.path.join(outdir, "static/pages"))]
     for dirs in staticdirs:
         for f in dirs[2]:
             suffix = pathlib.Path(f).suffix
@@ -264,26 +286,30 @@ if siteconfig.thumbnails:
 
             if siteconfig.staticexts[suffix] != "image":
                 continue
+
             img = Image.open(os.path.join(dirs[0], f))
-            if img.size[1] <= siteconfig.thumbsize:
+            if img.size[1] <= thumbsize:
                 continue
 
-            hratio = (siteconfig.thumbsize / float(img.size[1]))
+            hratio = (thumbsize / float(img.size[1]))
             wsize = int((float(img.size[0]) * float(hratio)))
 
-            logger.debug(f"Resizing {f} to {wsize}x{siteconfig.thumbsize}")
-            img = img.resize((wsize, siteconfig.thumbsize), Image.Resampling.BICUBIC)
+            logger.debug(f"Resizing {f} to {wsize}x{thumbsize}")
+            img = img.resize((wsize, thumbsize), Image.Resampling.BICUBIC)
 
             img.save(os.path.join(dirs[0], f"{nosuffix}_thumb{suffix}"))
 
 # Render styles
 for templatedir in [x for x in os.listdir(os.path.join(cfgdir, "templates"))]:
-    templates[templatedir] = Environment(loader = FileSystemLoader(os.path.join(cfgdir, "templates", templatedir, "css")), autoescape = select_autoescape())
+    templates[templatedir] = Environment(
+        loader = FileSystemLoader(os.path.join(cfgdir, "templates", templatedir, "css")),
+        autoescape = select_autoescape()
+    )
 
-os.makedirs("./out/static/styles")
+os.makedirs(os.path.join(outdir, "static/styles"))
 
 for name, theme in siteconfig.themes.items():
-    themepath = f"./out/static/styles/{name}.css"
+    themepath = os.path.join(outdir, f"static/styles/{name}.css")
 
     if not theme.enabled:
         logger.info(f"Theme \"{theme.dispname}\" ({name}) is disabled in config, skipping")
@@ -292,9 +318,8 @@ for name, theme in siteconfig.themes.items():
     template = templates[theme.templates].get_template("main.lis")
     css = template.render(theme = theme, themes = siteconfig.themes)
 
-    if siteconfig.minifycss:
+    if minifycss:
         css = minify_html.minify(css)
 
     with open(themepath, "w") as f:
         f.write(css)
-
